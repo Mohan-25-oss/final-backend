0980669d4e175407da157372f91ba030a836bce1
Mohan <bagan.halder@yahoo.com>
2025-10-02 23:11:57 +0800

    cors installed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('Home route is working!');
