c31c62f8eec1e652e73e2a4aec12f080e7f15f41
Mohan <bagan.halder@yahoo.com>
2025-10-02 22:22:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Home route is working!');
+});
+
+app.listen(port, () => {
+    console.log(`Booking backend listening at http://localhost:${port}`);
+});
